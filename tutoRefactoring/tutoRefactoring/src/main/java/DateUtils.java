575c253f1eca84ddb993df6589f894eeb3e197e3
Laura Nunez <lenrodriguez@iutbayonne.univ-pau.fr>
2026-03-11 15:25:54 +0100

    Refactoring of "DateUtils" class in tutoRefactoring

--- tutoRefactoring/tutoRefactoring/src/main/java/DateUtils.java
+++ tutoRefactoring/tutoRefactoring/src/main/java/DateUtils.java
@@ -3,7 +3,9 @@ import java.time.LocalDate;
 public class DateUtils {
     public static boolean isNowBetween(LocalDate startingDate, LocalDate endingDate) {
         LocalDate now = LocalDate.now();
-        return isaBoolean(startingDate, endingDate, now);
+        return now.isAfter(startingDate)
+                &&
+                now.isBefore(endingDate);
     }
 
     private static boolean isaBoolean(LocalDate startingDate, LocalDate endingDate, LocalDate now) {
